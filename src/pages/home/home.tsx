96a5ef8d7a9419842c8e57018778d4b427c8b57a
levyrodrigues23 <joselevyrodrigues23@gmail.com>
2025-03-18 20:36:16 -0300

    feat: requisição da api

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,13 +1,81 @@
 import styles from "./home.module.css";
 import { BsSearch } from "react-icons/bs";
+import { useState, FormEvent, useEffect } from "react";
+import { Link, useNavigate } from "react-router";
 
-import { Link } from "react-router";
+interface CoinProps {
+  id: string;
+  name: string;
+  simbol: string;
+  priceUsd: string;
+  vwap24Hr: string;
+  changePercent24Hr: string;
+  rank: string;
+  supply: string;
+  maxSupply: string;
+  marketCapUsd: string;
+  volumeUsd24Hr: string;
+  explorer: string;
+}
+
+interface DataProp {
+  data: CoinProps[];
+}
 
 function Home() {
+  const [input, setInput] = useState("");
+  const [coins, setCoins] = useState<CoinProps[]>([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    fetch("https://api.coincap.io/v2/assets?limit=10&offset=0")
+      .then((response) => response.json())
+      .then((data: DataProp) => {
+        const coinsData = data.data;
+
+const price = Intl.NumberFormat("en-US", {
+  style: 'currency',
+  currency: 'USD'
+})
+
+
+        const formatedResult = coinsData.map((item) => {
+          const formated = {
+            ...item,
+         formatedPrice: price.format(Number(item.priceUsd)),
+         formatedMarket: price.format(Number(item.marketCapUsd))
+          };
+
+          return formated;
+        });
+
+        console.log(formatedResult)
+      });
+  }
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    if (input === "") return;
+
+    navigate(`/detail/${input}`);
+  }
+
+  function handleGetMore() {}
+
   return (
     <main className={styles.container}>
-      <form className={styles.form}>
-        <input type="text" placeholder="digite o nome da moeda...Ex:BitCoin" />
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="digite o nome da moeda...Ex:BitCoin"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
         <button type="submit">
           <BsSearch size={30} color="#FFF" />
         </button>
@@ -47,6 +115,10 @@ function Home() {
           </tr>
         </tbody>
       </table>
+
+      <button className={styles.buttonMore} onClick={handleGetMore}>
+        Carregar mais...
+      </button>
     </main>
   );
 }
